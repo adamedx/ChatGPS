a0ee7630339a2e9df1e43ca80c32f98b569f159e
Adam Edwards <adamedx@gmail.com>
2023-11-28 19:28:21 -0800

    Include dependencies in lib directory

--- src/csharp/ChatGPS.cs
+++ src/csharp/ChatGPS.cs
@@ -1,9 +1,12 @@
 ﻿namespace Modulus.ChatGPS;
 
+using Microsoft.SemanticKernel;
+
 public class ChatGPS
 {
     public static int CreateSession()
     {
+        var builder = new KernelBuilder();
         return 3;
     }
 }
