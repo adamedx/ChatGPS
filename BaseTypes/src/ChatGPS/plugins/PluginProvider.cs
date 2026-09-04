36830c895c1b53e95fdd3c3c9e608db6fd9b08d6
Adam Edwards <adamedx@gmail.com>
2026-09-03 18:32:06 -0700

    SearchURL plugin support

--- BaseTypes/src/ChatGPS/plugins/PluginProvider.cs
+++ BaseTypes/src/ChatGPS/plugins/PluginProvider.cs
@@ -50,6 +50,7 @@ public abstract class PluginProvider
         PluginProvider.RegisterProvider(new StaticPluginProvider("TimePlugin", typeof(TimeNativePlugin), "Uses the local computer to obtain the current time.", null));
         PluginProvider.RegisterProvider(new StaticPluginProvider("FileIOPlugin", typeof(FileIONativePlugin), "Enables read and write access to the local file system.", null));
         PluginProvider.RegisterProvider(new StaticPluginProvider("TextPlugin", typeof(TextNativePlugin), "Allows the local computer to perform string manipulations.", null));
+        PluginProvider.RegisterProvider(new StaticPluginProvider("SearchUrlPlugin", typeof(SearchUrlNativePlugin), "Computes the search url for popular websites.", null));
         PluginProvider.RegisterProvider(new StaticPluginProvider("DocumentPlugin", typeof(DocumentNativePlugin), "Enables the ability to read the contents of Microsoft Word documents in the local file system.", null));
         PluginProvider.RegisterProvider(new HttpPluginProvider());
         PluginProvider.RegisterProvider(new BravePluginProvider());
